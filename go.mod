e873cad19db4404cd0d6c91510b7e7aaa857fd25
Joseph Barnett <jb@slim.ai>
2022-01-26 19:36:36 -0500

    committing reproduction case for localstack team

---
+++ go.mod
@@ -0,0 +1,9 @@
+module aws-go-eks
+
+go 1.13
+
+require (
+	github.com/pulumi/pulumi-aws/sdk/v4 v4.0.0
+	github.com/pulumi/pulumi-kubernetes/sdk/v3 v3.0.0
+	github.com/pulumi/pulumi/sdk/v3 v3.0.0
+)
---
+++ run_issue_reproduction.sh
@@ -0,0 +1,42 @@
+!/usr/bin/env bash
+
+set -euo pipefail
+
+die () {
+    echo "MISSING ${1}"
+    echo >&2 "$@"
+    exit 1
+}
+
+# Check some variables (need real keys)
+[ -z "${LOCALSTACK_API_KEY:-}" ] && { die "must have LOCALSTACK_API_KEY for pro features"}
+[ -z "${PULUMI_ACCESS_TOKEN:-}" ] && { die "must have PULUMI_ACCESS_TOKEN for pulumi automation in this repo"}
+
+# start localstack
+docker-compose up -d
+
+# get module dependancies (uncomment these just in case running as root)
+export PATH=$PATH:/usr/local/go/bin
+export GOPATH=$HOME/go
+export GOROOT=/usr/local/go
+go mod vendor
+
+# update pulumi
+curl -fsSL https://get.pulumi.com | sh
+export PATH=$PATH:/root/.pulumi/bin
+
+# setup fake keys for pulumi and localstack to use
+export AWS_DEFAULT_REGION=us-east-2
+export AWS_REGION=us-east-2
+export AWS_ACCESS_KEY_ID=test
+export AWS_SECRET_ACCESS_KEY=test
+
+# create a stack
+pulumi stack init dev
+
+# quick provision the pulumi config (with references to localstack)
+cp Pulumi.dev.yaml.bkup Pulumi.dev.yaml
+
+# run the deploy
+pulumi up -y
+
